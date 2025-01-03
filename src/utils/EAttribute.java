6e05d468362f3bff43c9a3b6f3e139574016a397
DESKTOP-EITGGEK\JiangMingNian <1564234202@qq.com>
2025-01-03 20:07:14 +0800

    使属性枚举更加清晰

--- src/utils/EAttribute.java
+++ src/utils/EAttribute.java
@@ -1,5 +1,11 @@
 package utils;
 
 public enum EAttribute {
-    ATK,AP,DEF,MR,SPEED,HP,MP//攻击 法强 防御 魔抗 速度 生命 蓝量
+    ATK,    // 攻击
+    AP,     // 法强
+    DEF,    // 防御
+    MR,     // 魔抗
+    SPEED,  // 速度
+    HP,     // 生命
+    MP      // 蓝量
 }
